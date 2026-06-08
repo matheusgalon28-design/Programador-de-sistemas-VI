a5868871594beeb40cd63c5e65fa798ae141a545
matheus <matheusgalon28@gmail.com>
2026-06-08 17:16:48 -0300

    06/08/2026

---
+++ churraasqueira.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+int main()
+{
+	int dia = 0;
+	
+	printf("favorinforme um número\n");
+	scanf("%d",&dia);
+	
+	switch(dia)
+	{
+		case1: 
+		printf("O numero informado resenda Domigo\n");
+		break;
+		case2:
+		printf("O numero informe reprenta Segunda-Feira\n");
+		break;
+		case3:
+		printf("O numero informado representa Terca-feira\n");
+		break;
+ 	    case4:
+		printf("O numero informado representa Quarta-Feira\n");
+		break;
+		case5:
+		printf("O numero informado reprenta Quinta-Feira\n");
+		case6:
+	    printf("o numero informado representa Sexta-Feira\n");
+		case7:
+		printf("O numero informado  representa Sabado\n");
+		break;
+		default:
+		printf(" o numero informado não reprenta número\n da semana);
+		
+	
+	
