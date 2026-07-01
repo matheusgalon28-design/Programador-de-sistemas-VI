b1148298c8eff299113904d5011bd14f316e67b0
matheusgalon28 <matheusgalon28@gmail.com>
2026-07-01 17:15:07 -0300

    trabalho

---
+++ trabalho.c
@@ -0,0 +1,107 @@
+#include<stdio.h>;
+{
+case1:
+printf("a=6,
+scanf("%lf",&a);
+printf("
+	
+	
+	
+	
+	
+}
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+printf("coloque seu nome-----------------------------\n");
+printf("BEM VINDO A CALCULADORA 1.0\n");
+printf(" 0  opcao MDC----------------------------------
+printf(" 1  opcao MMC----------------------------------
+printf(" 2  opcao soma---------------------------------
+printf(" 3  opcao
+printf(" 4  opcao 
+printf(" 5  opcao 
+printf(" 6  opcao 
+printf(" 7  opcao 
+printf(" 8  opcao 
+printf(" 9  opcao 
+printf(" 10 opcao  
+printf(" 11 opcao 
+printf("opção sair--------------------------------\n");
+printf("escolha uma opcao-------------------------\n");
+
+
+
+
+
+
+
+
+
+
+
